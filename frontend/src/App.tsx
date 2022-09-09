d3915815b2df4e6c5c756b18a26757696031dee1
WillianHora <wrsantos2002@gmail.com>
2022-09-08 21:39:12 -0300

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
-function App() {
-  return(
-    <h1>Olá mundo</h1>
-  )
+import Botao1 from "./components/notificationbutton"
+function App(){
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <Botao1/>
+      </> 
+       )
 }
 
 export default App
---
+++ frontend/src/components/notificationbutton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/poke.svg'
+function Botao1() {
+    return(
+      
+      <div className="meta-gf-btn">
+        <img src={icon} alt="butau" />
+      </div>
+    )
+  }
+  
+  export default Botao1
+  
