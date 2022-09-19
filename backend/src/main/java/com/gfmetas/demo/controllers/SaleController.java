62a6f04b30367bbe39cff76b1b75fc545d9c9931
WillianHora <wrsantos2002@gmail.com>
2022-09-18 21:38:10 -0300

    TWILLIO IMPLEMENTED

--- backend/src/main/java/com/gfmetas/demo/controllers/SaleController.java
+++ backend/src/main/java/com/gfmetas/demo/controllers/SaleController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gfmetas.demo.entities.Sale;
 import com.gfmetas.demo.services.SaleService;
+import com.gfmetas.demo.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -19,7 +21,9 @@ import com.gfmetas.demo.services.SaleService;
 public class SaleController {
     @Autowired
 	private SaleService service;
-	@GetMapping
+	@Autowired
+    private SmsService smsService;
+    @GetMapping
 	public Page<Sale> findSales(
 			@RequestParam (value="minDate", defaultValue ="")String minDate,
 			@RequestParam (value="maxDate", defaultValue ="")String maxDate,
@@ -28,5 +32,11 @@ public class SaleController {
 	 
 	 
  }
-
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+    	smsService.sendSms(id);
+    	 
+    	
+    	
+    }
 }
